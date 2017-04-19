eb4664e071420ab0bf75549a1c31af1166bac8e5
Lintang Timur <lintangtimur@protonmail.com>
2017-04-19 21:41:11 +0700

    release

--- drawing_mouse/Form1.Designer.cs
+++ drawing_mouse/Form1.Designer.cs
@@ -28,20 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(170, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Posisi";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(363, 261);
+            this.ClientSize = new System.Drawing.Size(481, 261);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Shining Drawing Equipment";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- drawing_mouse/Form1.cs
+++ drawing_mouse/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,57 @@ using System.Windows.Forms;
 
 namespace drawing_mouse
 {
+    /*
+     * Source : http://www.c-sharpcorner.com/forums/drawing-line-using-mouse-movement-event
+     * Edited : stelin
+     * 19 April 2017
+     */
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private Graphics g;
+        private Pen p;
+        private int prevX;
+        private int prevY;
+
+        private bool isDrawing = false;
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+            prevX = e.X;
+            prevY = e.Y;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+            Random rnd = new Random();
+
+            //Membuat random color berdasarkan RGB
+            p = new Pen(Color.FromArgb(rnd.Next(100), rnd.Next(256), rnd.Next(256)), 6);
+            if (isDrawing)
+            {
+                g.DrawLine(p, prevX, prevY, e.X, e.Y);
+                prevX = e.X;
+                prevY = e.Y;
+                label1.Text = $"X: {e.X.ToString()}, Y: {e.Y.ToString()}";
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = CreateGraphics();
         }
     }
 }
